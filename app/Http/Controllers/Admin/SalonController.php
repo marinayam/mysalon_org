1f40aca4c6173c36a5e0f8c383fbe7a1dc182d62
marina <marina5197216@gmail.com>
2021-11-28 23:34:52 +0900

    トップページヘッター　layouts/front

---
+++ app/Http/Controllers/Admin/SalonController.php
@@ -0,0 +1,25 @@
+<?php
+//管理者側のController
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class SalonController extends Controller
+{
+    //クライエント情報の一覧を表示させる
+    public function index(Request $request)
+    {
+        $posts = Salon::all()->sortByDesc('updated_at');
+
+        if (count($posts) > 0) {
+            $headline = $posts->shift();
+        } else {
+            $headline = null;
+        }
+
+        // salon/index.blade.php ファイルを渡している
+        // また View テンプレートに headline、 posts、という変数を渡している
+        return view('salon.index', ['headline' => $headline, 'posts' => $posts]);
+    }
+}
---
+++ app/Http/Controllers/SalonController.php
@@ -0,0 +1,15 @@
+<?php
+//一般ユーザー側のController
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+//追記。ファサード継承。
+use Illuminate\Support\Facades\HTML;
+
+// 追記。 以下を追記することでSalon Modelが扱えるようになる。
+use App\Salon;
+
+class SalonController extends Controller
+{
+     // 
+}
