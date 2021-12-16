1541ef2a79553c6b8d5376768e69ade1e17c96a7
marina <marina5197216@gmail.com>
2021-12-17 01:09:02 +0900

    パーマメニュー画面 perm

--- resources/views/perm.blade.php
+++ resources/views/perm.blade.php
@@ -5,54 +5,100 @@
 <link href="{{ asset('css/app.css') }}" rel ="stylesheet">
 {{-- 作成したCSSを追記 --}}
 <link href="{{ asset('css/perm.css') }}" rel="stylesheet">
-<div class="perm_content">
-    <div class="perm_ttl">
+<div class="container">
+    <div class="menu_page_head">
+        <img src="images/perm.png" class="d-block w-100">
+        <!--<div class="contents_ttl">-->
         <h2>
             <span>MENU & PRICE</span>
             <small>パーマ</small>
         </h2>
+    <!--</div>-->
     </div>
-    <section class="perm_content_menu">
-        <div class="container">
-            <div class="perm_content_price_list">
-                <div class="perm_price_item">
-                    <div class="perm_price_item_title">
-                        <h5>ラッシュリフト</h5>
-                        <div class="price_title_image">
-                            <img src="images/perm-round.png">
-                        </div>
-                    </div>
-                    <p class="recommend">根元から立ち上げすっぴんでも瞳を大きく！忙しい朝のメイク時間も短くできる♪</p>
-                    <div class="perm_price_item_table">
-                        <div class="color_table">
-                            <div class="row">
-                                <span class="col_left">上のみ(初回限定)</span>
-                                <span class="col_right">¥5,500</span>
+    <section class="perm_menu">
+        <div class="inner inner--sm inner-padding--lg">
+            <h3>ラッシュリフト</h3>
+            <p class="recommend_ttl">
+                根元から立ち上げでナチュラルに瞳を大きく
+            </p>
+            <div class="perm_price">
+                <ul class="perm_price_table">
+                    <li class="perm_price_item">
+                        <div class="c_flex_column_half">
+                            <div class="left c_flex_column__item">
+                                <p class="item_name">
+                                    上まつげのみ(初回限定)
+                                </p>
+                                <p class="item_detail">所要時間：45分</p>
                             </div>
-                            <div class="row">
-                                <span class="col_left">上のみ</span>
-                                <span class="col_right">¥6,600</span>
-                            </div>
-                            <div class="row">
-                                <span class="col_left">上下(初回限定)</span>
-                                <span class="col_right">¥8,800</span>
-                            </div>
-                            <div class="row">
-                                <span class="col_left">上下</span>
-                                <span class="col_right">¥11,000</span>
+                            <div class="right c_flex_column__item">
+                                <p class="item_price">5,500
+                                <i>円（税込）</i>
+                                </p>
                             </div>
                         </div>
-                    </div>
-                    <div class="menu-price-item-compare">
-                        <div class="compare-item">
-                            <p class="normal">逆さまつ毛でお悩みの方や、まぶたが重くて瞳を大きく見せたい方におすすめ！</p>
+                    </li>
+                    <li class="perm_price_item">
+                        <div class="c_flex_column_half">
+                            <div class="left c_flex_column__item">
+                                <p class="item_name">
+                                    上まつげのみ（リピートのお客様）
+                                </p>
+                                <p class="item_detail">所要時間：45分</p>
+                            </div>
+                            <div class="right c_flex_column__item">
+                                <p class="item_price">5,500
+                                <i>円（税込）</i>
+                                </p>
+                            </div>
                         </div>
-                    </div>
-                </div>
-            </div>
+                    </li>
+                    <li class="perm_price_item">
+                        <div class="c_flex_column_half">
+                            <div class="left c_flex_column__item">
+                                <p class="item_name">
+                                    上下（初回限定）
+                                </p>
+                                <p class="item_detail">所要時間：80分</p>
+                            </div>
+                            <div class="right c_flex_column__item">
+                                <p class="item_price">8,800
+                                <i>円（税込）</i>
+                                </p>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="perm_price_item">
+                        <div class="c_flex_column_half">
+                            <div class="left c_flex_column__item">
+                                <p class="item_name">
+                                    上下（リピートのお客様）
+                                </p>
+                                <p class="item_detail">所要時間：80分</p>
+                            </div>
+                            <div class="right c_flex_column__item">
+                                <p class="item_price">11,000
+                                <i>円（税込）</i>
+                                </p>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </div> 
+        </div>
+        <div class="menu-price-item-recommend">
+            <ul class="recommend-point">
+                <li>
+                    ○逆さまつ毛でお悩みの方
+                </li>
+                <li>
+                    ○まぶたが重くて瞳を大きく見せたい方
+                </l>
+                <li>
+                    ○メンテナンスが簡単
+                </l>
+            </ul>
         </div>
     </section>
 </div>
-
-
 @endsection
