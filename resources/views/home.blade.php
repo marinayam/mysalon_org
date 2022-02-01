8f461d70d675ee92d24195402d01edefc0ae4590
marina <marina5197216@gmail.com>
2022-02-02 01:04:24 +0900

    home bladeデザイン編集

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,12 @@
-@extends('layouts.app')
+@extends('layouts.front')
 
+@section('title','マイページ')
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">マイページ</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,7 +15,7 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    ご予約
                 </div>
             </div>
         </div>
